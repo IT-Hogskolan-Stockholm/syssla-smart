cb2fa5a65a48848923faf9f74bccbee91d726a38
xintic <oskar.sjobeck@gmail.com>
2025-02-17 18:01:40 +0100

    #3 updated to camelCase. Left JSON-files in snake_case for potential future use.

--- syssla-smart/src/stores/ChoreStore.js
+++ syssla-smart/src/stores/ChoreStore.js
@@ -7,161 +7,161 @@ export const useChoreStore = defineStore('choreStore', {
         id: 1,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 2,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 3,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 4,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 5,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 6,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 7,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 8,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 9,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 10,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 11,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 12,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 13,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 14,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 15,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 16,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 17,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 18,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 19,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       },
       {
         id: 20,
         title: '',
         deadline: '2025-03-14',
-        assigned_to: '',
-        is_completed: false,
-        point_value: 1
+        assignedTo: '',
+        isCompleted: false,
+        pointValue: 1
       }
     ]
   })
--- syssla-smart/src/stores/UserStore.js
+++ syssla-smart/src/stores/UserStore.js
@@ -6,32 +6,32 @@ export const useUserStore = defineStore('userStore', {
       {
         id: 1,
         name: '',
-        score_value: 1,
-        completed_tasks: 0
+        scoreValue: 1,
+        completedTasks: 0
       },
       {
         id: 2,
         name: '',
-        score_value: 1,
-        completed_tasks: 0
+        scoreValue: 1,
+        completedTasks: 0
       },
       {
         id: 3,
         name: '',
-        score_value: 1,
-        completed_tasks: 0
+        scoreValue: 1,
+        completedTasks: 0
       },
       {
         id: 4,
         name: '',
-        score_value: 1,
-        completed_tasks: 0
+        scoreValue: 1,
+        completedTasks: 0
       },
       {
         id: 5,
         name: '',
-        score_value: 1,
-        completed_tasks: 0
+        scoreValue: 1,
+        completedTasks: 0
       }
     ]
   })
