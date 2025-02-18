b4fb2128c68d7fb9572de1de1c7e69d8aaab94f7
Breanna Sand <breanna.sand8@gmail.com>
2025-02-18 13:18:29 +0100

    template for ny syssla button and dialog

--- syssla-smart/src/views/Sysslor.vue
+++ syssla-smart/src/views/Sysslor.vue
@@ -2,7 +2,25 @@
   <div>
     <h1>Sysslor</h1>
     <p>Lista över sysslor...</p>
+    <div class="skapa-syssla">
+      <v-btn @click="openDialog" color="primary">Skapa Syssla</v-btn>
+    </div>
+
+    <v-dialog v-model="dialog" max-width="400px">
+      <v-card>
+        <v-card-text>
+          <v-text-field v-model="choreName" label="Titel"</v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn @click="createChore" color="primary">Lägg Till</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+  const openDialog = () => {
+    console.log("Create chore clicked!")
+  }
+</script>
