a11bd53c0dd5de64dc8ff9166dcfe3173bf4e6c2
NAgnekil <nathalie.agnekil@gmail.com>
2025-02-24 00:10:07 +0100

    changed IDs on users

--- syssla-smart/src/stores/UserStore.js
+++ syssla-smart/src/stores/UserStore.js
@@ -11,21 +11,21 @@ export const useUserStore = defineStore('userStore', () => {
       completedTasks: 0
     },
     {
-      id: 1,
+      id: 2,
       name: 'PA',
       color: '#7CB342',
       scoreValue: 1,
       completedTasks: 0
     },
     {
-      id: 1,
+      id: 3,
       name: 'AL',
       color: '#039BE5',
       scoreValue: 1,
       completedTasks: 0
     },
     {
-      id: 1,
+      id: 4,
       name: 'SO',
       color: '#EC407A',
       scoreValue: 1,
