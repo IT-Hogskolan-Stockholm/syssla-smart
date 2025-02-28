4ac2393e1762e26049aa811f7f6bca76fca38838
Ruben-Mostrom <ruben.mostrom@gmail.com>
2025-02-28 12:17:20 +0100

    Update Sync.vue

--- src/components/Sync.vue
+++ src/components/Sync.vue
@@ -1,8 +1,25 @@
 <script setup>
   import { ref } from 'vue'
 
+  const houseCode = ref('')
   const loading = ref(false)
   const synced = ref(false)
+  const codeValid = ref(false)
+
+  const rules = [
+    (value) => {
+      if (value) return true
+      return 'Du måste skriva in en Hushållskod'
+    },
+    (value) => {
+      if (value.toLowerCase() === 'hsh1234') {
+        codeValid.value = true 
+        return true
+      }
+      codeValid.value = false 
+      return 'Hittar inget hushåll med den koden'
+    },
+  ]
 
   const startLoading = () => {
     loading.value = true
@@ -30,14 +47,20 @@
         class="d-flex flex-column"
         style="min-height: 220px; border-radius: 20px; box-shadow: 10px 10px 15px rgba(0, 0, 0, 0.3)"
       >
-        <v-card-text>
-          <v-text-field label="Hushållskod"></v-text-field>
-        </v-card-text>
+        <v-form @submit.prevent>
+          <v-card-text>
+            <v-text-field 
+              v-model="houseCode" 
+              :rules="rules"
+              label="Hushållskod"
+            />
+          </v-card-text>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn text="Synka ->" @click="startLoading"></v-btn>
-        </v-card-actions>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text="Synka ->" type="submit" @click="startLoading" :disabled="!houseCode || !codeValid"></v-btn>
+          </v-card-actions>
+        </v-form>
       </v-card>
 
       <v-card
