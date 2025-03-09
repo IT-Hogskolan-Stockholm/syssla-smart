442705fe8ffcb4a7220e3725ec99fc3eae487751
xintic <oskar.sjobeck@gmail.com>
2025-03-09 20:48:17 +0100

    Small fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Sysslor from '../views/Sysslor.vue'
-import Beloningar from '../views/Beloningar.vue'
+import Belöningar from '../views/Belöningar.vue'
 import Historik from '../views/Historik.vue'
 import Inställningar from '../views/Inställningar.vue'
 
 const routes = [
   { path: '/', component: Sysslor },
-  { path: '/beloningar', component: Beloningar },
+  { path: '/beloningar', component: Belöningar },
   { path: '/historik', component: Historik },
   { path: '/installningar', component: Inställningar }
 ]
